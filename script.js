67f4352b8e6eb57f06f344139c7db97de4924f35
RawlingsX <chrisrawlings566@gmail.com>
2024-12-05 23:33:05 +0100

    basic web page set up

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById("clickButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Hello, you've clicked the button!";
+});
